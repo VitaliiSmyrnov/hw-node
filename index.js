48708fcd63901c154810326658a3df98f1d88e08
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2023-04-30 15:36:13 +0300

    step 4 is done

--- index.js
+++ index.js
@@ -5,26 +5,39 @@ const {
   addContact,
 } = require("./contacts");
 
+const { program } = require("commander");
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
       const allContacts = await listContacts();
-      return console.log(allContacts);
+      return console.table(allContacts);
     // break;
 
     case "getById":
       const oneContact = await getContactById(id);
-      return console.log(oneContact);
+      return console.table(oneContact);
     // break;
 
     case "add":
       const newContact = await addContact(name, email, phone);
-      return console.log(newContact);
+      return console.table(newContact);
     // break;
 
     case "remove":
       const deletedContact = await removeContact(id);
-      return console.log(deletedContact);
+      return console.table(deletedContact);
     // break;
 
     default:
