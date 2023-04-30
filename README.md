695ffe55670fb5b688103eb5568b78b04399e3fe
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2023-04-30 16:23:28 +0300

    step 5 and 6 are done

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # hw-node
+
+Screenshots:
+
+Отримуємо і виводимо весь список контактів у вигляді таблиці:
+https://prnt.sc/twe6MMIBEihB
+
+Отримуємо контакт по id:
+https://prnt.sc/yS5KLXLiqcPI
+
+Додаємо контакт:
+https://prnt.sc/cmw7KeN1-3mH
+
+Видаляємо контакт:
+https://prnt.sc/RXMVc3TEhtG-
--- index.js
+++ index.js
@@ -22,23 +22,23 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
       const allContacts = await listContacts();
-      return console.table(allContacts);
-    // break;
+      console.table(allContacts);
+      break;
 
-    case "getById":
+    case "get":
       const oneContact = await getContactById(id);
-      return console.table(oneContact);
-    // break;
+      console.table(oneContact);
+      break;
 
     case "add":
       const newContact = await addContact(name, email, phone);
-      return console.table(newContact);
-    // break;
+      console.table(newContact);
+      break;
 
     case "remove":
       const deletedContact = await removeContact(id);
-      return console.table(deletedContact);
-    // break;
+      console.table(deletedContact);
+      break;
 
     default:
       console.warn("\x1B[31m Unknown action type!");
