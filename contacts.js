d40b3c44e858b40d7732f5734ce6d0e58f51cd43
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2023-04-30 14:59:32 +0300

    step 2 is done

---
+++ contacts.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "contacts.json");
+
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
+
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const findContact = contacts.find((item) => item.id === contactId);
+  return findContact || null;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+
+  if (index === -1) return null;
+
+  const [deletedContact] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return deletedContact || null;
+};
+
+const addContact = async (name, email, phone) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
